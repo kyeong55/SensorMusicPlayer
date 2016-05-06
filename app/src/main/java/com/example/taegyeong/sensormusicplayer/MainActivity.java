839e8a99715c7c31c664b02a7da1e8122ffd459b
Taegyeong <kyeong102@gmail.com>
2016-05-06 21:13:12 +0900

    Added Tone Generator

--- app/src/main/java/com/example/taegyeong/sensormusicplayer/MainActivity.java
+++ app/src/main/java/com/example/taegyeong/sensormusicplayer/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.taegyeong.sensormusicplayer;
 
+import android.graphics.Typeface;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Typeface branBlack;
+    public Typeface branBold;
+    public Typeface branRegular;
+    public Typeface branLight;
+
+    private final int duration = 3; // seconds
+    private final int sampleRate = 8000;
+    private final int numSamples = duration * sampleRate;
+    private final double sample[] = new double[numSamples];
+    private final double freqOfTone = 440; // hz
+
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        branBlack = Typeface.createFromAsset(getAssets(), "brandon_blk.otf");
+//        branBold = Typeface.createFromAsset(getAssets(), "brandon_bld.otf");
+//        branRegular = Typeface.createFromAsset(getAssets(), "brandon_med.otf");
+//        branLight = Typeface.createFromAsset(getAssets(), "brandon_reg.otf");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Use a new tread as this can take a while
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+        thread.start();
+    }
+
+    void genTone(){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound(){
+        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioTrack.MODE_STATIC);
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
     }
 }
